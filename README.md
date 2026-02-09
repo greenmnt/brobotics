3d163e5b0a3a7325ebc4b24e0358b045ffabf60b
offshore <offshore>
2026-02-09 18:10:24 +1000

    port the quaternion binary from c++

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 socat -d -d \
   PTY,raw,echo=0,link=/tmp/imu_in \
   PTY,raw,echo=0,link=/tmp/imu_out
+
+
+  MPU6050 githubs
+   - https://github.com/ZHomeSlice/Simple_MPU6050
+   - https://github.com/jrowberg/i2cdevlib/blob/master/Arduino/MPU6050/examples/MPU6050_DMP6/MPU6050_DMP6.ino
---
+++ scripts/run_mpu6050_with_tee.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+set -euo pipefail
+
+# Start socat in background
+socat PTY,raw,echo=0,link=/tmp/imu_in PTY,raw,echo=0,link=/tmp/imu_out &
+SOCAT_PID=$!
+
+# Kill socat when runner exits
+trap "kill $SOCAT_PID" EXIT
+NAME=mpu6050
+RUSTFLAGS="-C link-arg=-Tlink.x" cargo b -p $NAME --release --target thumbv7em-none-eabihf 
+probe-rs run --chip STM32F334C8 --no-timestamps target/thumbv7em-none-eabihf/release/$NAME "$@" | tee /tmp/imu_in
+
